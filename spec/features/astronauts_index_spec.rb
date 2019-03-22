8e3b11fbb648e0e5e4d2490e7f26f9c6adabd2cc
Matt Levy <milevy1@gmail.com>
2019-03-22 10:31:05 -0600

    Added user story 2

--- spec/features/astronauts_index_spec.rb
+++ spec/features/astronauts_index_spec.rb
@@ -2,30 +2,39 @@ require 'rails_helper'
 
 RSpec.describe 'Astronauts index page', type: :feature do
   context 'When I visit /astronauts' do
-    it 'shows a list of astronaut information' do
-      neil = Astronaut.create(
+    before(:each) do
+      @neil = Astronaut.create(
         name: 'Neil Armstrong',
         age: 37,
         job: 'Commander'
       )
-      matt = Astronaut.create(
+      @matt = Astronaut.create(
         name: 'Matt Levy',
         age: 27,
         job: 'Computer Programmer'
       )
-      visit "/astronauts"
+      visit astronauts_path
+    end
 
-      within "#astronaut-#{neil.id}" do
+    it 'shows a list of astronaut information' do
+      within "#astronaut-#{@neil.id}" do
         expect(page).to have_content('Name: Neil Armstrong')
         expect(page).to have_content('Age: 37')
         expect(page).to have_content('Job: Commander')
       end
 
-      within "#astronaut-#{matt.id}" do
+      within "#astronaut-#{@matt.id}" do
         expect(page).to have_content('Name: Matt Levy')
         expect(page).to have_content('Age: 27')
         expect(page).to have_content('Job: Computer Programmer')
       end
     end
+
+    it 'shows average age of all astronauts' do
+      within "#average-age" do
+        expect(page).to have_content('Average age: 32')
+      end
+    end
+
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,24 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'Instance methods' do
+    describe '.average_age' do
+      it 'returns the average age of astronauts' do
+        Astronaut.create(
+          name: 'Neil Armstrong',
+          age: 37,
+          job: 'Commander'
+        )
+        Astronaut.create(
+          name: 'Matt Levy',
+          age: 27,
+          job: 'Computer Programmer'
+        )
+        astronauts = Astronaut.all
+
+        expect(astronauts.average_age).to eq(32)
+      end
+    end
+  end
 end
